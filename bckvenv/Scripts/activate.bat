1d2a36bb16d0a8082b7828dfe7d5b96777351967
Mckgare0717 <mckinnonrebello@gmail.com>
2023-11-10 10:45:40 +0000

    tried add workouts

--- bckvenv/Scripts/activate.bat
+++ bckvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\40173492\Desktop\New folder\titanicFitnessBackend-1\bckvenv
+set VIRTUAL_ENV=C:\Users\40173492\Desktop\New folder\titanicFitnessBackend\bckvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(bckvenv) 
 
 :END
 if defined _OLD_CODEPAGE (
