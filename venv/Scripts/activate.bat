c3d4fbf3cbf576788374b888546c3b56c15a128a
Mckgare0717 <mckinnonrebello@gmail.com>
2023-11-17 10:55:05 +0000

    same

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\40173492\Desktop\New folder\titanicFitnessBackend\venv
+set VIRTUAL_ENV=C:\Users\40173492\Desktop\New folder (2)\titanicFitnessBackend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
